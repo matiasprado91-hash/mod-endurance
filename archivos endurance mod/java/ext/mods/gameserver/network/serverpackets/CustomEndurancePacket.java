029a8f79990c3383913ddc2a492dcfcbbc904cdd
Dr. Miles Bennett Dyson <matiasprado91@gmail.com>
2026-09-22 18:43:39 -0300

    Add custom endurance update packet

---
+++ archivos endurance mod/java/ext/mods/gameserver/network/serverpackets/CustomEndurancePacket.java
@@ -0,0 +1,23 @@
+package ext.mods.gameserver.network.serverpackets;
+
+public class CustomEndurancePacket extends L2GameServerPacket
+{
+	private static final int OPCODE_TUTORIAL_SHOW_HTML = 0xA6;
+	private static final String HEADER = "ENDURANCE_UPDATE";
+
+	private final int _objectId;
+	private final int _endurance;
+
+	public CustomEndurancePacket(int objectId, int endurance)
+	{
+		_objectId = objectId;
+		_endurance = endurance;
+	}
+
+	@Override
+	protected void writeImpl()
+	{
+		writeC(OPCODE_TUTORIAL_SHOW_HTML);
+		writeS(HEADER + "|ObjectID=" + _objectId + "|Endurance=" + _endurance);
+	}
+}
